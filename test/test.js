50ab8561c61ed74fd39cebaf3df3f042d867a18f
Carlos Castro <alu0100819847@ull.edu.es>
2017-05-16 22:20:34 +0100

    Añadidos modulos

--- test/test.js
+++ test/test.js
@@ -5,6 +5,9 @@ describe("Función comparar", function(){
     var result = comparar("Primos(5)");
     result.should.be.equal("1, 2, 3, 5, 7");
   })
+  it("La función comparar debe devolver el resultado de la solicitud de secuencia", function() {
+    var result = comparar("Primos(5) Igual Primos(5)");
+    result.should.be.equal("1, 2, 3, 5, 7");
+  })
 
 })
-//var regexp = /([a-zA-Z]*)\(((?:\d)*)\)(?:\s)*(?:([a-zA-Z]*)(?:\s)*([a-zA-Z]*)\((\d)\))?/
