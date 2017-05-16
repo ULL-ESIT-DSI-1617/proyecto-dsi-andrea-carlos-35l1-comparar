ad3814f9a5fba0d14f60af82754416518449ebdc
Carlos Castro <alu0100819847@ull.edu.es>
2017-05-16 20:09:29 +0100

    Añadido index.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
-var Igual = require('../lib/Igual.js');
+var Igual = require('../lib/comparar.js');
 
 describe("Función comparar", function(){
   it("La función comparar debe devolver el resultado de la solicitud de secuencia", function() {
-
+    var result = comparar("Primos(5)");
+    result.should.be.equal("1, 2, 3, 5, 7");
   }
 
 })
