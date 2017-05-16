ad1491e7ba1984b4a793aaa41a862b167f7df971
Carlos Castro <alu0100819847@ull.edu.es>
2017-05-16 22:02:01 +0100

    Añadidos modulos

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-var Igual = require('../lib/comparar.js');
-
+var comparar = require('../lib/comparar.js');
+var should = require('should');
 describe("Función comparar", function(){
   it("La función comparar debe devolver el resultado de la solicitud de secuencia", function() {
     var result = comparar("Primos(5)");
     result.should.be.equal("1, 2, 3, 5, 7");
-  }
+  })
 
 })
+//var regexp = /([a-zA-Z]*)\(((?:\d)*)\)(?:\s)*(?:([a-zA-Z]*)(?:\s)*([a-zA-Z]*)\((\d)\))?/
