c55980296124a951fe97b179a5eafa9093ae1586
Carlos Castro <alu0100819847@ull.edu.es>
2017-05-17 00:31:36 +0100

    Resuelto problema al añadir nuevas operaciones

--- test/test.js
+++ test/test.js
@@ -9,5 +9,8 @@ describe("Función comparar", function(){
     var result = comparar("Primos(5) Igual Primos(5)");
     result.should.be.equal("1, 2, 3, 5, 7");
   })
-
+  it("La función comparar con la operacion diferencia debe devolver el resultado de la solicitud de secuencia", function() {
+    var result = comparar("Primos(6) Diferencia Primos(5)");
+    result.should.be.equal("11");
+  })
 })
